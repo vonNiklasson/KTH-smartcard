b9246579c68acf49a62d76c459dc3c7c629baca0
Johan Niklasson <johan@nicklassons.se>
2017-05-03 15:01:57 +0200

    Fixed order of code.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
+#pragma config |= 0x00D4
+#define MAX_STRING 16
+
 #include "16F690.h"
 #include "initialize.c"
 #include "str.c"
-
-#pragma config |= 0x00D4
-#define MAX_STRING 16
 
 void main(void) {
     initialize();
