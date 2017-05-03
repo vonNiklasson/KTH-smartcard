f16aa6c0dbd1a45c7ccaa2c8afb62d637fae0a3d
Johan Niklasson <johan@nicklassons.se>
2017-05-03 15:06:25 +0200

    Added comments and fixed bug.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
+/* This file contains the main 
+ * function and the program logic */
+
+/* First include of chipkit-header */
+#include "16F690.h"
+
 #pragma config |= 0x00D4
 #define MAX_STRING 16
 
-#include "16F690.h"
+/* Include extended functions */
 #include "initialize.c"
 #include "str.c"
 
